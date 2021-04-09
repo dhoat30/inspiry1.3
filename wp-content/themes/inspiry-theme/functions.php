f55c5531cc46f2f942aca36b36b2fbed33a2e911
Gurpreet <dhoat30@gmail.com>
2021-04-09 19:03:57 +0530

    added data layer in woocommerce archive page

--- wp-content/themes/inspiry-theme/functions.php
+++ wp-content/themes/inspiry-theme/functions.php
@@ -32,7 +32,7 @@ require get_theme_file_path('/inc/nav-registeration.php');
       wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.aebecbb789db7969773b.js'),  array( 'jquery' ), '1.0', true);
       wp_enqueue_script('main', get_theme_file_uri('/bundled-assets/scripts.a4db89b3d11487f9346e.js'), NULL, '1.0', true);
       wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.a4db89b3d11487f9346e.css'));      
-      wp_enqueue_style('our-vendor-styles', get_theme_file_uri('/bundled-assets/styles.a4db89b3d11487f9346e.css'));
+      wp_enqueue_style('our-vendor-styles', get_theme_file_uri('/bundled-assets/styles.aebecbb789db7969773b.css'));
 
     }
     wp_localize_script("main", "inspiryData", array(
